91830c6630e036b2cccb128d21f7ea62beb23dca
a4004 <88835216+a4004@users.noreply.github.com>
2024-11-28 15:23:22 +0000

    added status info to footer

--- order332-com/src/routes/+layout.svelte
+++ order332-com/src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import { ModeWatcher } from "mode-watcher";
 	import logo_svg from "$lib/images/polygon-icon.svg";
 	import "../app.css";
-    import Navbar from "$lib/modules/navigation/navbar.svelte";
-    import type { NavbarProps } from "$lib/modules/navigation/navbar-props";
+	import Navbar from "$lib/modules/navigation/navbar.svelte";
+	import type { NavbarProps } from "$lib/modules/navigation/navbar-props";
 	let { children } = $props();
 
 	const navdata = {
@@ -11,23 +11,23 @@
 			{
 				text: "About 332",
 				url: "/about-us",
-				align: "middle"
+				align: "middle",
 			},
 			{
 				text: "Discord",
 				url: "https://discord.gg/technology",
-				align: "middle"
+				align: "middle",
 			},
 			{
 				text: "Publications",
 				url: "/publications",
-				align: "middle"
+				align: "middle",
 			},
 			{
 				text: "Gallery",
 				url: "/gallery",
-				align: "middle"
-			}
+				align: "middle",
+			},
 		],
 	} satisfies NavbarProps;
 </script>
@@ -38,22 +38,43 @@
 </svelte:head>
 
 <ModeWatcher defaultMode="dark" />
-<Navbar navdata={navdata}/>
+<Navbar {navdata} />
 <div>
 	{@render children()}
 </div>
 <footer class="border-t border-primary/10 shadow mt-24">
 	<div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
 		<div class="sm:flex sm:items-center sm:justify-between">
-			<a href="https://order332.com" class="flex items-center mb-4 sm:mb-0">
+			<a
+				href="https://order332.com"
+				class="flex items-center mb-4 sm:mb-0"
+			>
 				<img src={logo_svg} class="h-8 mr-3" alt="332 Logo" />
-				<span class="self-center text-xl font-semibold whitespace-nowrap">332</span>
+				<span
+					class="self-center text-xl font-semibold whitespace-nowrap"
+					>332</span
+				>
 			</a>
-			<ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-primary sm:mb-0">
-
+			<ul
+				class="flex flex-wrap items-center mb-6 text-sm font-medium text-primary sm:mb-0"
+			>
+				<iframe
+					title="Service Status"
+					src="https://332.instatus.com/embed-status/7759da4f/dark-sm"
+					height="40"
+					frameBorder="0"
+					scrolling="no"
+					style="border: none;"
+					class="w-full rounded-lg"
+				>
+				</iframe>
 			</ul>
 		</div>
 		<hr class="my-6 border-primary/10 sm:mx-auto lg:my-8" />
-		<span class="block text-sm text-primary sm:text-center mb-1">© 2024 <a href="https://order332.com" class="hover:underline">332</a>. All Rights Reserved.</span>
+		<span class="block text-sm text-primary sm:text-center mb-1"
+			>© 2024 <a href="https://order332.com" class="hover:underline"
+				>332</a
+			>. All Rights Reserved.</span
+		>
 	</div>
 </footer>
